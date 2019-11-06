2b6166d1fd95ee153ab2f2aa66392cd191f652c8
mohammad-alhawamdeh <mohammadsalman152@yahoo.com>
2019-11-06 14:41:40 +0200

    m

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+var answer = prompt('do you love car( If yes press y if no press n)');
+var Massege ;
+
+if (answer === 'y'){
+  Massege = 'lets go to see car';
+}
+else {
+  Massege = 'whats you doing her ?';
+}
+
+document.write ('<h1>' + Massege + '</h1>');
