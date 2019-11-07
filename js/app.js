5d1679b9a0b566e6924970ebd594e5c6fb4d704c
mohammad-alhawamdeh <mohammadsalman152@yahoo.com>
2019-11-07 10:54:22 +0200

    this is edit

--- js/app.js
+++ js/app.js
@@ -12,3 +12,18 @@ else {
 }
 
 document.write ('<h1>' + Massege + '</h1>');
+
+
+document.write ('<h1>' + 'good mornning ' + '</h1>');
+var answer1 = prompt('how many car did you want');
+
+var Massege1 ;
+
+if (answer1 === 'y'){
+  Massege1 = 'lets go to see car';
+}
+else {
+  Massege1 = 'whats you doing her ?';
+}
+
+document.write ('<h1>' + Massege1 + '</h1>');
